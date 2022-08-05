c19e1597b66d8440199b7db565497b49ced74325
Aayush <aayush.gtr619@gmail.com>
2022-08-05 23:37:23 +0530

    push

--- src/main/java/com/example/demo/service/ProductExcelProcessStateService.java
+++ src/main/java/com/example/demo/service/ProductExcelProcessStateService.java
@@ -1,18 +1,7 @@
 package com.example.demo.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.example.demo.entity.ProductExcelProcessState;
-import com.example.demo.repository.ProductExcelProcessStateRepository;
 
-@Service
-public class ProductExcelProcessStateService {
 
-	@Autowired
-	private ProductExcelProcessStateRepository excelProcessStateRepository;
-
-	public ProductExcelProcessState getProductProcessStatus(Long productProcessId) {
-		return excelProcessStateRepository.findById(productProcessId).get();
-	}
+public interface ProductExcelProcessStateService {
+	ProductExcelProcessState getProductProcessStatus(Long productProcessId);
 }
